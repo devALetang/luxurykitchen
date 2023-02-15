ecb2534a5c5157fd26c493ec511fa67a32d133c3
devALetang <121792082+devALetang@users.noreply.github.com>
2023-02-15 15:56:39 +0100

    Add files via upload

---
+++ luxury/js/script.js
@@ -0,0 +1,17 @@
+function panier() {
+    var element = document.getElementById("misenpanier");
+    if (element.style.display === "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
+
+function popsupp() {
+    var element = document.getElementById("misenpanier");
+    if (element.style.display === "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
